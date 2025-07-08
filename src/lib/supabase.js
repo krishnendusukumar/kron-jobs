75c705b1c82cb8fc1fca8ad586fd0e08376b676a
krishnendusukumar <krishnendu.1923co1112@kiet.edu>
2025-07-09 02:22:56 +0530

    build fix

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -2,4 +2,4 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.supabase = void 0;
 var supabase_js_1 = require("@supabase/supabase-js");
-exports.supabase = (0, supabase_js_1.createClient)(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+exports.supabase = (0, supabase_js_1.createClient)(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
