839705adf04cf519b0d0b1982646e52955252554
krishnendusukumar <krishnendu.1923co1112@kiet.edu>
2025-07-18 00:49:06 +0530

    added speedinsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./theme-provider";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -87,6 +88,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeProvider>
+          <SpeedInsights />
           {children}
         </ThemeProvider>
       </body>
