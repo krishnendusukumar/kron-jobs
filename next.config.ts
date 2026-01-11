fa89c2f29f180bf300c3b111c99dcfe1712b2b52
krishnendusukumar <krishnendu.1923co1112@kiet.edu>
2026-01-12 02:15:24 +0530

    add nextjs latest version:15~latest

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  experimental: {
-    appDir: true,
-  },
+
   typescript: {
     // Ignore TypeScript errors during build for now
     ignoreBuildErrors: true,
